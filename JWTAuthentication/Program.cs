f0e2b1ed35827f6bf9d5da51eadbf732bd7113b7
Jana Saa <jana.abusaa@gmail.com>
2024-11-14 22:24:21 +0200

    Add Interfaces

--- JWTAuthentication/Program.cs
+++ JWTAuthentication/Program.cs
@@ -1,25 +1,68 @@
-var builder = WebApplication.CreateBuilder(args);
+using JWTAuthentication;
+using JWTAuthentication.Controllers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        ConfigureServices(builder);
+
+        var app = builder.Build();
+        Configure(app);
+    }
+
+
+    private static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            var key = Encoding.UTF8.GetBytes(builder.Configuration["JWTToken:Key"]);
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidIssuer = builder.Configuration["JWTToken:Issuer"],
+                ValidAudience = builder.Configuration["JWTToken:Audience"],
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
+        builder.Services.AddSingleton(new JwtTokenGenerator(builder.Configuration["JWTToken:Key"]));
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+
+    private static void Configure(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
