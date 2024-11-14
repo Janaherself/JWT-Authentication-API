dd8572b4c84cc69ac5135521aa9abd0ac87113a8
Jana Saa <jana.abusaa@gmail.com>
2024-11-14 22:23:00 +0200

    Create Unauthenticated endpoint

---
+++ JWTAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController(JwtTokenGenerator tokenGenerator) : ControllerBase
+    {
+        private readonly JwtTokenGenerator _tokenGenerator = tokenGenerator;
+
+        [HttpGet]
+        public IActionResult Login(UserLogin request)
+        {
+            if (IsValidUser(request.Username, request.Password))
+            {
+                var token = _tokenGenerator.GenerateToken(request.Username);
+                return Ok(new { Token = token });
+            }
+            return Unauthorized();
+        }
+
+        private bool IsValidUser(string username, string password)
+        {
+            return username == "guest" && password == "guest";
+        }
+    }
+}
